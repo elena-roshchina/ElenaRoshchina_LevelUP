53666019568659c0910d51432f6352eeaf05185f
elena-roshchina <star-fox@yandex.ru>
2020-11-19 23:22:12 +0300

    Change initial class Program just to modify something and to see

--- src/main/java/ru/levelup/elena/roshchina/qa/Program.java
+++ src/main/java/ru/levelup/elena/roshchina/qa/Program.java
@@ -3,5 +3,6 @@ package ru.levelup.elena.roshchina.qa;
 public class Program {
     public static void main(String[] args){
         System.out.println("Hello Git!");
+        System.out.println("Hello Git, again!");
     }
 }
